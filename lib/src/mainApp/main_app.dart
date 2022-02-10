ed5a64beac46bc717e0dc29724eeb5ff8863a96f
Salar Pro <salarkhalid97@gmail.com>
2022-02-10 17:55:33 +0300

    done

--- lib/src/mainApp/main_app.dart
+++ lib/src/mainApp/main_app.dart
@@ -17,17 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Container(
         child: ElevatedButton(
           child: Text("salar"),
-          onPressed: () async {
-            Map<String, dynamic> salar = {};
-            salar["hello"] = 153;
-            salar["salar"] = true;
-            salar["pro"] = 1.5;
-
-            FirebaseFirestore.instance
-                .collection("messages")
-                .doc("2aad3f70-72ef-11ec-9f53-27ae9b057fd6")
-                .set({"name": 123});
-          },
+          onPressed: () async {},
         ),
       )),
     );
